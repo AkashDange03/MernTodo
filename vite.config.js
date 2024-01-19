6ef49a273af967603c911c4aa39ea12782667bb3
Akash_03 <akash.dange0307@gmail.com>
2024-01-19 13:16:30 +0530

    proxy changed to none

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    proxy:{
-      "/api/v1":"https://todobackend-7nto.onrender.com"
-    }
+    // proxy:{
+    //   "/api/v1":"https://todobackend-7nto.onrender.com"
+    // }
   },
   plugins: [react()],
  
